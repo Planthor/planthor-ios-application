93702a77f3cd28835093bbd87f7482b0ad883b1d
Luu Phat <luuphatdev@outlook.com>
2026-05-23 11:26:03 +0700

    feat: setup navigation scaffold and initial screens

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -5,12 +5,12 @@ class AppColors {
   static const Color forestGreen = Color(0xFF2D5A27);
   static const Color leafGreen = Color(0xFF4CAF50);
   static const Color sageGreen = Color(0xFF8DAA91);
-  
+
   // Neutral/Earth Tones
   static const Color earthBrown = Color(0xFF4E342E);
   static const Color cream = Color(0xFFF5F5DC);
   static const Color offWhite = Color(0xFFFAFAFA);
-  
+
   // Functional Colors
   static const Color error = Color(0xFFD32F2F);
   static const Color success = Color(0xFF388E3C);
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:planthor_ios_application/features/navigation/presentation/main_scaffold.dart';
 import 'core/theme/app_theme.dart';
 
 void main() {
   // ProviderScope is required for Riverpod to work
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
@@ -22,55 +19,8 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
       title: 'Planthor',
       theme: theme,
-      home: const MyHomePage(title: 'Planthor Home'),
+      home: const MainScaffold(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
